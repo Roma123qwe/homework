d2bc9f1538ecf587dcdfae0eb8ef8e876470b8e6
Roma <romamuht@gmail.com>
2021-05-22 13:01:36 +0300

    commit

--- books_shop/urls.py
+++ books_shop/urls.py
@@ -1,8 +1,11 @@
+import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("shop/", include("sales_manager.urls")),
-    path('hotel/', include('hotel_room.urls'))
+    path('hotel/', include('hotel_room.urls')),
+    path('account/', include('sales_manager.urls')),
+    path('__debug__/', include(debug_toolbar.urls)),
 ]
